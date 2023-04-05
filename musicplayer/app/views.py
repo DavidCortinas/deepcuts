414e6152af753c24568e9fbe6fa7224b4151f5ba
davidcortinas <dcortinas33@gmail.com>
2023-04-05 13:09:31 -0500

    music player build with html js frontend and python django backend

---
+++ musicplayer/app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.core.paginator import Paginator
+from . models import Song
+
+# Create your views here.
+def index(request):
+    paginator= Paginator(Song.objects.all(),1)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context={'page_obj': page_obj}
+
+    return render(request, 'index.html', context)
